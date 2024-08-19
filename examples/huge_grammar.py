21c4643a87343ef682bbe603be16d5dfac47c302
Hichem Ammar Khodja <hichem.ammarkhodja@orange.com>
2024-08-19 19:10:44 +0200

    renaming

--- examples/huge_grammar.py
+++ examples/huge_grammar.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from choice_concat_parser import ChoicesConcatenationParser, end_symb
+from multi_choice_parser import MultiChoiceParser, end_symb
 
 
 l = np.random.randint(0, 10**9, 1000000).astype(str)
@@ -8,13 +8,13 @@ l = [
     ['the', 'an', "a", ""],
     l
 ]
-p = ChoicesConcatenationParser(l)
+p = MultiChoiceParser(l)
 
 to_parse = l[1][0]
 print('String to parse: "%s"\n\n' % to_parse)
 for i, c in enumerate(tuple(to_parse) + (end_symb, )):
     print('Step %s' % i)
-    print("Authorized characters :", sorted(p.next()))
+    print("Authorized characters:", sorted(p.next()))
     print('Adding character:', c)
     p.step(c)
     print("State: Finished=%s, Success=%s" % (p.finished, p.success))
