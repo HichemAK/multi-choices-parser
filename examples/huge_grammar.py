1b0f82fad523b1fa4d39b902293640c936f3a892
Hichem Ammar Khodja <hichem.ammarkhodja@orange.com>
2024-08-25 16:47:29 +0200

    Hierarchical alphabet first tests passed!
    - Might need to rethink the way grammars are represented to provide a better syntax

--- examples/huge_grammar.py
+++ examples/huge_grammar.py
@@ -5,13 +5,13 @@ from multi_choices_parser import MultiChoicesParser, end_symb
 # install numpy if needed
 l = np.random.randint(0, 10**9, 1000000).astype(str)
 l = [
-    ['the', 'an', "a", ""],
-    l
+    [tuple(x,) for x in ['the', 'an', "a", ""]],
+    [tuple(x,) for x in l]
 ]
 p = MultiChoicesParser(l)
 
 to_parse = l[1][0]
-print('String to parse: "%s"\n\n' % to_parse)
+print('String to parse: "%s"\n\n' % (to_parse,))
 for i, c in enumerate(tuple(to_parse) + (end_symb, )):
     print('Step %s' % i)
     print("Authorized characters:", sorted(p.next()))
